d0b5436d9dedf1e6221c046c4f96757f9a0809b6
Andrew Galkin <andrushka@MacBook-Air-Andrew.local>
2021-08-25 15:34:38 +0300

    update js structure

--- js/fetchCountries.js
+++ js/fetchCountries.js
@@ -20,21 +20,23 @@ const refs = {
   countriesContent: document.querySelector(".articles")
 }
 
-refs.mainSearchInput.addEventListener("change", debounce(countrySearchInput, 500))
-
-function countrySearchInput(event) {
-  event.preventDefault();
+// refs.mainSearchInput.addEventListener("change", debounce(countrySearchInput, 500))
+refs.mainSearchInput.oninput = debounce((event) => {
   clearCountriesContent();
   const searchTypes = event.target.value;
 
   fetchCountries(searchTypes).then(data => {
     if (data.length > 10) {
       error({
-        text: "Too many matches found. Please enter a more specific query!"
+        text: "Too many matches found. Please enter a more specific query!",
+        delay: 1000,
+
       });
     } else if (data.status === 404) {
       error({
-        text: "No country has been found. Please enter a more specific query!"
+        text: "No country has been found. Please enter a more specific query!",
+        delay: 1500,
+
       });
     } else if (data.length === 1) {
       findCountry(data, country);
@@ -47,7 +49,35 @@ function countrySearchInput(event) {
         text: "You must enter query parameters!"
       });
     })
-}
+
+},500)
+
+// function countrySearchInput(event) {
+//   event.preventDefault();
+//   clearCountriesContent();
+//   const searchTypes = event.target.value;
+//
+//   fetchCountries(searchTypes).then(data => {
+//     if (data.length > 10) {
+//       error({
+//         text: "Too many matches found. Please enter a more specific query!"
+//       });
+//     } else if (data.status === 404) {
+//       error({
+//         text: "No country has been found. Please enter a more specific query!"
+//       });
+//     } else if (data.length === 1) {
+//       findCountry(data, country);
+//     } else if (data.length <= 10) {
+//       findCountry(data, countries);
+//     }
+//   })
+//     .catch(error => {
+//       error({
+//         text: "You must enter query parameters!"
+//       });
+//     })
+// }, 500)
 
 function findCountry(countries, template) {
   const markup = countries.map(count => template(count)).join();
