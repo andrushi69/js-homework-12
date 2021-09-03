b4e9c92d82a285addf437deee19517f57ab8ffab
Andrew Galkin <andrushka@MacBook-Air-Andrew.local>
2021-09-04 01:02:18 +0300

    delet comma

--- js/fetchCountries.js
+++ js/fetchCountries.js
@@ -8,6 +8,7 @@ import countries from "/templates/countries.hbs"
 defaultModules.set(PNotifyMobile, {});
 
 
+
 const mainLink = 'https://restcountries.eu/rest/v2/name/';
 const fetchCountries = (countries) => {
   const listOfCountries = `${countries}`;
@@ -52,35 +53,8 @@ refs.mainSearchInput.oninput = debounce((event) => {
 
 },500)
 
-// function countrySearchInput(event) {
-//   event.preventDefault();
-//   clearCountriesContent();
-//   const searchTypes = event.target.value;
-//
-//   fetchCountries(searchTypes).then(data => {
-//     if (data.length > 10) {
-//       error({
-//         text: "Too many matches found. Please enter a more specific query!"
-//       });
-//     } else if (data.status === 404) {
-//       error({
-//         text: "No country has been found. Please enter a more specific query!"
-//       });
-//     } else if (data.length === 1) {
-//       findCountry(data, country);
-//     } else if (data.length <= 10) {
-//       findCountry(data, countries);
-//     }
-//   })
-//     .catch(error => {
-//       error({
-//         text: "You must enter query parameters!"
-//       });
-//     })
-// }, 500)
-
 function findCountry(countries, template) {
-  const markup = countries.map(count => template(count)).join();
+  const markup = countries.map(count => template(count)).join("");
   refs.countriesContent.insertAdjacentHTML('afterbegin', markup)
 }
 
